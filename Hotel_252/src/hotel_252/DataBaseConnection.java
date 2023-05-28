c5c042d927f204824a11df8d46901fdeca964014
Abdualaziz Alsharif <62527536+AbdulazizAlsharif@users.noreply.github.com>
2023-05-29 02:35:24 +0300

    fix

--- Hotel_252/src/hotel_252/DataBaseConnection.java
+++ Hotel_252/src/hotel_252/DataBaseConnection.java
@@ -1,30 +1,30 @@
-
 package hotel_252;
 
 import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
 public class DataBaseConnection {
+
     // Singielton its only one connection to DB
     private static DataBaseConnection connnection;
     private Connection Con;
+
     private DataBaseConnection() {
         try {
-<<<<<<< Updated upstream
-            this.Con=DriverManager.getConnection("jdbc:mariadb://localhost:3306/hotel_252", "root", "2040569");
-=======
-            this.Con=DriverManager.getConnection("jdbc:mariadb://localhost:3306/hotel_252", "root", "2036023");
->>>>>>> Stashed changes
+
+            this.Con = DriverManager.getConnection("jdbc:mariadb://localhost:3306/hotel_252", "root", "2040569");
+
+            this.Con = DriverManager.getConnection("jdbc:mariadb://localhost:3306/hotel_252", "root", "2036023");
+
         } catch (SQLException ex) {
             Logger.getLogger(DataBaseConnection.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    public static DataBaseConnection getconnnection(){
+
+    public static DataBaseConnection getconnnection() {
         if (connnection == null) {
-            connnection =new DataBaseConnection();
+            connnection = new DataBaseConnection();
         }
         return connnection;
     }
@@ -32,5 +32,5 @@ public class DataBaseConnection {
     public Connection getCon() {
         return Con;
     }
-    
+
 }
