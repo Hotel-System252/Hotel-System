0bde9581734fcbc18b0a2f931c29f38eda0fd401
Omar Alzahrani <99991447+Ozh07@users.noreply.github.com>
2023-05-28 23:10:05 +0300

    Update BreakFast+Valet Class

--- Hotel_252/src/hotel_252/BreakFast.java
+++ Hotel_252/src/hotel_252/BreakFast.java
@@ -10,5 +10,29 @@ package hotel_252;
  * @author Nero
  */
 public class BreakFast {
-    
+
+    String Breakfast;
+    int Price;
+
+    public BreakFast(String BreakFast, int Price) {
+        this.Breakfast = BreakFast;
+        this.Price = Price;
+    }
+
+    public String getBreakfast() {
+        return Breakfast;
+    }
+
+    public void setBreakfast(String Breakfast) {
+        this.Breakfast = Breakfast;
+    }
+
+    public int getPrice() {
+        return Price;
+    }
+
+    public void setPrice(int Price) {
+        this.Price = Price;
+    }
+
 }
--- Hotel_252/src/hotel_252/Valet.java
+++ Hotel_252/src/hotel_252/Valet.java
@@ -10,5 +10,30 @@ package hotel_252;
  * @author Nero
  */
 public class Valet {
-    
+
+    String Valet;
+    int Price;
+
+    public Valet(String Valet, int Price) {
+        this.Valet = Valet;
+        this.Price = Price;
+
+    }
+
+    public String getValet() {
+        return Valet;
+    }
+
+    public void setValet(String Valet) {
+        this.Valet = Valet;
+    }
+
+    public int getPrice() {
+        return Price;
+    }
+
+    public void setPrice(int Price) {
+        this.Price = Price;
+    }
+
 }
